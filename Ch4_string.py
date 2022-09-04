75d4fe7042e419a924e554c7b731b2a85904696d
Chynmn <chynmn0523@gmail.com>
2022-09-04 17:43:24 +0900

    문자열 처리 함수

--- Ch4_string.py
+++ Ch4_string.py
@@ -21,3 +21,34 @@ print("뒤 7자리 : " + jumin[7:])   # 7부터 끝까지
 print("뒤 7자리 (뒤에부터) : " + jumin[-7:])   # 맨 뒤에서 7번째부터 끝까지
 
 # 문자열 처리 함수
+python = "Python is Amazing"
+print(python.lower())   # 소문자 출력 함수
+print(python.upper())   # 대문자 출력 함수
+print(python[0].isupper())  # 대문자 확인하는 함수
+print(len(python))      # 문자열 길이 함수
+print(python.replace("Python", "Java"))     # 값 대체 함수
+
+index = python.index("n")   # 문자의 위치를 알려주는 함수
+print(index)
+index = python.index("n", index + 1)    # 2번째 n을 찾는 함수
+print(index)
+
+print(python.find("n"))     # 문자의 위치를 알려주는 함수
+print(python.find("Java"))  # 찾는 값이 없으면 -1을 반환
+# print(python.index("Java")) # index는 찾는 값이 없으면 프로그램 오류가 뜸
+
+print(python.count("n"))    # 문자가 몇번 사용 됐는지 알려주는 함수
+
+# 문자열 포맷
+
+
+
+
+
+
+
+
+
+
+
+
