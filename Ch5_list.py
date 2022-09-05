b5b315ed91cbff4b5a15b19d1ae0861ec8c2053d
Chynmn <chynmn0523@gmail.com>
2022-09-05 18:34:49 +0900

    Tuple

--- Ch5_list.py
+++ Ch5_list.py
@@ -101,6 +101,26 @@ cabinet.clear()
 print(cabinet)
 
 
+# 사전 Tuple : List와 다르게 내용 변경이나 추가가 불가능. 단, 속도가 빠르다.
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생성까스") -> 변경, 추가 불가
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
+
+
+
+
+
+
 
 
 
