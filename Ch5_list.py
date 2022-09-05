cf08cbf84cf878facf72711eb2ac48471d397421
Chynmn <chynmn0523@gmail.com>
2022-09-05 21:32:33 +0900

    Quiz 4

--- Ch5_list.py
+++ Ch5_list.py
@@ -1,9 +1,9 @@
 # 리스트 []
 
 # 지하철 칸별로 10명, 20명, 30명
-subway1 = 10
-subway2 = 20
-subway3 = 30
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
 
 subway = [10, 20, 30]
 print(subway)
@@ -158,4 +158,46 @@ print(menu, type(menu))
 menu = set(menu)
 print(menu, type(menu))
 
+
+# Quiz 4) 당신의 학교에서는 파이썬 코딩 대회를 주최합니다.
+"""
+참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다.
+댓글 작성자등 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+추첨 프로그램을 작성하기오.
+
+조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20이라고 가정
+조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+조건3 : random 모듈의 shuffle 과 sample 을 활용
+
+(출력 예제)
+ -- 당첨자 발표 --
+치킨 당첨자 : 1
+커피 당첨자 : [2, 3, 4]
+ -- 축하 합니다 --
+ 
+ (활용 예제)
+ from random import*
+ lst = [1, 2, 3, 4, 5]
+ print(lst)
+ shuffle(lst)
+ print(lst)
+ print(sample(lst, 1))
+"""
+
+from random import *
+
+users = range(1, 21)    # 1부터 20까지 숫자를 생성
+# print(type(users))
+users = list(users)
+# print(type(users))
+
+print(users)
+shuffle(users)
+print(users)
+
+winners = sample(users, 4)  # 4명 중 1명은 치킨, 3명은 커피
+print("-- 당첨자 발표 --")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print("-- 축하 합니다 --")
 
