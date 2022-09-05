d17445848c79dbff7b8f22198fd6844a5d8cdc20
Chynmn <chynmn0523@gmail.com>
2022-09-05 21:14:42 +0900

    자료구조의 변경

--- Ch5_list.py
+++ Ch5_list.py
@@ -145,3 +145,17 @@ java.remove("김태호")
 print(java)
 
 
+# 자료구조의 변경
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
+
+
