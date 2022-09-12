91911f84a2d0a034f8abe849cf4379450089afec
Chynmn <chynmn0523@gmail.com>
2022-09-12 18:37:25 +0900

    continue break 한 줄 for문

--- Ch6_if_for_while.py
+++ Ch6_if_for_while.py
@@ -49,10 +49,31 @@
 #     print("{0}, 커피가 준비 되었습니다.".format(customer))
 #     person = input("이름이 어떻게 되세요? ")
 
-# continue
+# continue와 break
+absent = [2, 5]     # 결석 -> 다음 반복으로
+no_book = [7]       # 책을 안가져옴 -> 반복문 종료
+for student in range(1, 11):
+    if student in absent:
+        continue
+    elif student in no_book:
+        print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
+        break
+    print("{0}, 책을 읽어봐".format(student))
 
+# 한 줄 for
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
+students = [1, 2, 3, 4, 5]
+print(students)
+students = [i + 100 for i in students]      # students를 i에 넣고 100을 더해라
+print(students)
 
-# break
+# 학생 이름을 길이로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students)
 
-
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
 
