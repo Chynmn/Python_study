2d46b3e333e4645cece750833b61abdc028258df
Chynmn <chynmn0523@gmail.com>
2022-09-12 18:26:29 +0900

    if문 for문 while문

--- Ch6_if_for_while.py
+++ Ch6_if_for_while.py
@@ -1,12 +1,58 @@
-# if 분기
-weather = input("오늘 날씨는 어떄요? ")
-if weather == "비" or weather == "눈":
-    print("우산을 챙기세요")
-elif weather == "미세먼지":
-    print("마스크를 챙기세요")
-else:
-    print("준비물 필요 없어요")
+# if 조건문
 
+# weather = input("오늘 날씨는 어떄요? ")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요")
+
+# temp = int(input("기온은 어때요? "))  # 입력받은 문자열을 정수형으로 바꿔줌
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨예요")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지 마세요")
+
+# for 반복문
+# for waiting_no in range(1, 6):
+#     print("대기번호 : {0}".format(waiting_no))
+#
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+
+# while
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비되었습니다. {1}번 남았어요.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분 되었습니다.")
+
+# 무한반복
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비 되었습니다. 호출 {1}회".format(customer, index))
+#     index += 1
+
+# while문 탈출
+# customer = "토르"
+# person = "Unknown"
+# while person != customer:
+#     print("{0}, 커피가 준비 되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요? ")
+
+# continue
+
+
+# break
 
 
 
