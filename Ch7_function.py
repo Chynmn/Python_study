0e96fe5e7f25ca959a95cfb6a26ff2eaa71c217f
Chynmn <chynmn0523@gmail.com>
2022-09-19 19:56:18 +0900

    지역변수 전역변수 Quiz 7

--- Ch7_function.py
+++ Ch7_function.py
@@ -66,6 +66,9 @@ profile2(main_lang="자바", age=25, name="김태호")
 #     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
 #     print(lang1, lang2, lang3, lang4, lang5)
 
+# profile3("유재석", 20, "Python", "Java", "C", "C#", "C++")
+# 함수의 매개변수 인자를 가변적으로 쓰기 위함
+
 def profile3(name, age, *language):
     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
     for lang in language:
@@ -73,4 +76,59 @@ def profile3(name, age, *language):
     print()
 
 
-profile3("유재석", 20, "Python", "Java", "C", "C#", "Swift")
+profile3("유재석", 20, "Python", "Java", "C", "C#", "C++", "JavaScript")
+profile3("김태호", 25, "Kotlin", "Swift")
+
+
+# 지역변수와 전역변수
+gun = 10
+
+
+def checkpoint(soldiers):  # 경계근무
+    global gun  # 전역 공간에 있는 gun 사용
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+
+
+# 전역변수를 남용하면 코드 관리가 어려워지므로 권장하지 않음. 따라서 전달값과 반환값을 이용한 방법을 사용
+def checkpoint_ret(gun, soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+
+
+print("전체 총 : {0}".format(gun))
+# checkpoint(2)   # 두 명이 경계근무 나감
+gun = checkpoint_ret(gun, 2)
+print("남은 총 : {0}".format(gun))
+
+
+# Quiz 7) 표준 체중을 구하는 프로그램을 작성하시오
+"""
+표준 채중 : 각 개인의 키에 적당한 체중
+
+(성별에 따른 공식)
+남자 : 키(m) x 키(m) x 22
+여자 : 키(m) x 키(m) x 21
+
+조건1 : 표준 체중은 별도의 함수 내에서 계산
+    - 함수명 : std_weight
+    - 전달값 : 키(height), 성별(gender)
+조건2 : 표준 체중은 소수점 둘째자리까지 표시
+
+(출력 예제)
+키 175cm 남자의 표준 체중은 67.38kg 입니다.
+"""
+
+
+def std_weight(height, gender):     # 키 m 단위(실수), 성별 "남자"  /   "여자"
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
+
+
+height = 170  # cm 단위
+gender = "남자"
+weight = round(std_weight(height / 100, gender), 2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
