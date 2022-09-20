15df0018b3883e1c4494e202634e2b66284a2809
Chynmn <chynmn0523@gmail.com>
2022-09-20 22:18:38 +0900

    메소드

--- Ch9_class.py
+++ Ch9_class.py
@@ -41,18 +41,46 @@ class Unit:
         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
 
-marine1 = Unit("마린", 40, 5)       # self를 제외한 부분을 입력
-marine2 = Unit("마린", 40, 5)       # Unit class의 인스턴스
-tank = Unit("탱크", 150, 35)        # 함수에 정의된 정보를 모두 입력해야 함
+# marine1 = Unit("마린", 40, 5)       # self를 제외한 부분을 입력
+# marine2 = Unit("마린", 40, 5)       # Unit class의 인스턴스
+# tank = Unit("탱크", 150, 35)        # 함수에 정의된 정보를 모두 입력해야 함
 
-# 레이스 : 공중 유닛, 비행기, 클로킹 (상대방에겐 보이지 않음)
-wraith1 = Unit("레이스", 80, 5)     # 클래스의 생성자를 통해 작성
-# 클래스 외부에서 멤버변수를 통해 사용
-print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+# # 레이스 : 공중 유닛, 비행기, 클로킹 (상대방에겐 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)      # 클래스의 생성자를 통해 작성
+# # 클래스 외부에서 멤버변수를 통해 사용
+# print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
 
-# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
-wraith2 = Unit("빼앗은 레이스", 80, 5)
-wraith2.clocking = True
+# # 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("빼앗은 레이스", 80, 5)
+# wraith2.clocking = True             # 클래스 외부에서 객체에 추가로 변수를 만들어 쓸 수 있음
 
-if wraith2.clocking == True:
-    print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
+# if wraith2.clocking == True:
+#     print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+
+# 메소드 - 공격유닛
+class AttackUnit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    def attack(self, location):         # 메소드 앞에는 항상 self
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"
+              .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+
+# 파이어뱃 : 공격 유닛, 화염방사기
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
