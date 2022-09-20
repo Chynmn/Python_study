4f7e4409f5354c2e40eb5bc7b0f27ca565f29426
Chynmn <chynmn0523@gmail.com>
2022-09-20 22:02:10 +0900

    클래스 멤버변수

---
+++ Ch9_class.py
@@ -0,0 +1,58 @@
+# # Starcraft
+# # 마린 : 공격 유닛, 군인, 총을 쏠 수 있음
+# name = "마린"
+# hp = 40
+# damage = 5
+
+# print("{} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# # 탱크 : 공격 유닛, 탱크, 포를 쏠 수 있는데, 일반 모드 / 시즈 모드
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+# tank2_name = "탱크"
+# tank2_hp = 150
+# tank2_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank2_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
+
+
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(
+#         name, location, damage))
+
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# attack(tank2_name, "1시", tank2_damage)
+
+class Unit:
+    def __init__(self, name, hp, damage):   # __init__ : 생성자. 객체가 생성될 때 호출
+        self.name = name                    # 멤버 변수 : 클래스 내에서 정의된 변수
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+
+marine1 = Unit("마린", 40, 5)       # self를 제외한 부분을 입력
+marine2 = Unit("마린", 40, 5)       # Unit class의 인스턴스
+tank = Unit("탱크", 150, 35)        # 함수에 정의된 정보를 모두 입력해야 함
+
+# 레이스 : 공중 유닛, 비행기, 클로킹 (상대방에겐 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)     # 클래스의 생성자를 통해 작성
+# 클래스 외부에서 멤버변수를 통해 사용
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
