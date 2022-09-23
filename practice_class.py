046b406c15fd4f3b08646ecee6dca2eff045062d
Chynmn <chynmn0523@gmail.com>
2022-09-23 19:54:49 +0900

    클래스 실습

---
+++ practice_class.py
@@ -0,0 +1,13 @@
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+
+
+class FlyableUnit(Unit, Flyable):
+    def __init__(self):
+        super().__init__()
